09e62cbd114d61f465c39bf598a72dbdf880f1e3
jkukko <joonas.kukkonen@gmail.com>
2020-05-16 12:59:52 +0300

    remove inventory functionality added

--- application/inventory/views.py
+++ application/inventory/views.py
@@ -1,6 +1,10 @@
 from application import app, db
-from flask import render_template, request
+from flask import redirect, render_template, request, url_for
 from application.inventory.models import Inventory
+
+@app.route("/inventories", methods=["GET"])
+def inventory_index():
+    return render_template("inventory/list.html", inventories = Inventory.query.all())
 
 @app.route("/inventory/new/")
 def inventory_form():
@@ -13,4 +17,14 @@ def inventory_create():
     db.session().add(i)
     db.session.commit()
 
-    return "hello world!"
+    return redirect(url_for("inventory_index"))
+
+@app.route("/inventory/<inventory_id>/", methods=["POST"])
+def inventory_remove(inventory_id):
+
+    i = Inventory.query.get(inventory_id)
+
+    db.session.delete(i)
+    db.session.commit()
+
+    return redirect(url_for("inventory_index"))
