e847c4fbf9ee1d1d56b53487f1a3e44daebddc32
jkukko <joonas.kukkonen@gmail.com>
2020-05-23 11:41:54 +0300

    changes

--- application/inventory/models.py
+++ application/inventory/models.py
@@ -6,5 +6,7 @@ class Inventory(db.Model):
     date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),onupdate=db.func.current_timestamp())
     name = db.Column(db.String(144), nullable=False)
 
+    # change
     def __init__(self, name):
         self.name = name
+
