3f329f3d141e579c5656ef903edcb495e183736d
jkukko <joonas.kukkonen@gmail.com>
2020-05-25 22:41:37 +0300

    product model updated

--- application/product/models.py
+++ application/product/models.py
@@ -8,11 +8,11 @@ class Product(db.Model):
     name = db.Column(db.String(144), nullable=False)
     segment = db.Column(db.String(144), nullable=False)
     current_stock = db.Column(db.Integer)
-    safetyStock = db.Column(db.Integer)
+    safety_stock = db.Column(db.Integer)
 
     def __init__(self, name, segment):
         self.name = name
         self.segment = segment
         self.current_stock = 0
-        self.safetyStock = 0
+        self.safety_stock = 0
 
