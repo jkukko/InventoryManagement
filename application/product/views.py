1a2ed2b2b684a48d13f0ff5662c676d0a687c159
jkukko <joonas.kukkonen@gmail.com>
2020-05-25 23:56:05 +0300

    product view updated

--- application/product/views.py
+++ application/product/views.py
@@ -25,6 +25,8 @@ def product_create():
         return render_template("product/new.html", form = form)
 
     p = Product(form.name.data, form.segment.data)
+
+    # This is just test
     p.inventory_id = 1
 
     db.session().add(p)
