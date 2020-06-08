03e3d06996b509154bc82fbddead597b19da3c11
jkukko <joonas.kukkonen@gmail.com>
2020-06-08 22:42:54 +0300

    user creation updated

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -1,9 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField
+from wtforms import PasswordField, StringField, validators
   
 class LoginForm(FlaskForm):
     username = StringField("Username")
     password = PasswordField("Password")
   
+    class Meta:
+        csrf = False
+
+class CreateAccountForm(FlaskForm):
+    username = StringField("Username", [validators.Length(min=3, max=15, message='Username must be 3-15 characters')])
+    password = PasswordField("Password", [validators.Length(min=3, max=15, message='Password must be 3-15 characters')])
+
     class Meta:
         csrf = False
--- application/auth/views.py
+++ application/auth/views.py
@@ -3,7 +3,7 @@ from flask_login import login_user, logout_user
   
 from application import app, db
 from application.auth.models import User
-from application.auth.forms import LoginForm
+from application.auth.forms import LoginForm, CreateAccountForm
 
 @app.route("/auth/login", methods = ["GET", "POST"])
 def auth_login():
@@ -27,21 +27,49 @@ def auth_logout():
     logout_user()
     return redirect(url_for("index"))
 
+
 @app.route("/auth/new/")
 def user_form():
-    return render_template("/auth/new.html", form = LoginForm())
+    return render_template("/auth/new.html", form = CreateAccountForm())
 
 @app.route("/auth/", methods=["POST"])
 def user_create():
-    form = LoginForm(request.form)
+    form = CreateAccountForm(request.form)
 
     if not form.validate():
-        render_template("/auth/new.html", form = form)
+        return render_template("auth/new.html", form = form)
     
-    print(form.password.data)
+    userExist = User.query.filter_by(username = form.username.data).first()
+    if userExist:
+        return render_template("auth/new.html", form = form, error = "Username is taken")
+
     u = User(username=form.username.data, password=form.password.data)
 
     db.session().add(u)
     db.session().commit()
+
+    return redirect(url_for("index"))
+
+
+@app.route("/auth/new/", methods=["GET", "POST"])
+def auth_register():
+
+    if request.method == "GET":
+        return render_template("auth/new.html", form = CreateAccountForm())
+
+    form = CreateAccountForm(request.form)
+
+    if not form.validate():
+        return render_template("auth/new.html", form = form)
+
+    oldUser = User.query.filter_by(username = form.username.data).first()
+
+    if oldUser:
+        return render_template("auth/new.html", form = form, error = "Username is taken")
+
+    user = User(form.username.data, form.password.data)
+
+    db.session.add(user)
+    db.session.commit()
 
     return redirect(url_for("index"))
